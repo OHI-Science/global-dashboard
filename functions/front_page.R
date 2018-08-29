e8feb7855cdb7bd465c67183a2adba5e2a5ba894
iwensu0313 <isu@bren.ucsb.edu>
2018-08-29 15:10:10 -0700

    fixed up home page so it's more complete / presentable - get feedback on content, visuals, aesthetics next Tues!!

--- functions/front_page.R
+++ functions/front_page.R
@@ -14,6 +14,8 @@ frontp = function()
       div(class = "intro-divider"), 
       
       # Additional information
+      tags$p("The Ocean Health Index is a tailorable marine assessment framework to comprehensively and quantitatively evaluate ocean health. Originally developed by an interdisciplinary team of scientists, global assessments have been repeated every year since 2012."),
+      
       tags$p("Currently, we only have data exploration available for our Mariculture sub-goal, but the remaining 10 goals will be available in the near future. Other goals include Tourism & Recreation, Clean Waters, Biodiversity, Artisanal Opportunities, Carbon Storage, and Natural Products."),
       
       # Two hyperlink boxes: more info
@@ -22,15 +24,15 @@ frontp = function()
           tags$a(target = "_blank",
                  href = "http://ohi-science.org/about/#what-is-the-ocean-health-index",
                  div(class = "float box box-more",
-                     tags$p(class = "intro-text", "The Basics"),
+                     tags$p(class = "intro-text", "Overview"),
                      tags$p("What is the Ocean Health Index?")
                      )),
           
           tags$a(target = "_blank",
                  href = "http://ohi-science.org/ohi-global/",
                  div(class = "float box box-rear",
-                     tags$p(class = "intro-text", "Find out more"),
-                     tags$p("The Ocean Health Index Global Assessment is produced from a collaborative team of scientists based in Santa Barbara.") 
+                     tags$p(class = "intro-text", "Deep Dive"),
+                     tags$p("The Ocean Health Index Global Assessment is produced from a collaborative team of scientists based in Santa Barbara. Click here for documentation of methods, data, and publications.") 
                      ))
           )
       )
